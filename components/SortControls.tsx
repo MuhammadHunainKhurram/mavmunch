4af759fbd0da6ddf2c6c9a8704dc0dbcab7c6f8d
ali-jifi <alijifi12@gmail.com>
2026-02-15 23:55:54 -0600

    sort labels added for mobile

--- components/SortControls.tsx
+++ components/SortControls.tsx
@@ -33,7 +33,7 @@ export function SortControls({ currentSort, onSortChange }: SortControlsProps) {
               }`}
             >
               {icon}
-              <span className="hidden sm:inline">{label}</span>
+              <span>{label}</span>
             </button>
           );
         })}
